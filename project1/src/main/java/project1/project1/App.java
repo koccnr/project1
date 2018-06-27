cdf9a9042d709fa9bcc7bc95390c55ea7532e89b
e1 <Caner.Koc-CIC.Germany@ibm.com>
2018-06-28 00:53:58 +0200

    println deleted

--- project1/src/main/java/project1/project1/App.java
+++ project1/src/main/java/project1/project1/App.java
@@ -11,9 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "selam arkadas" );
-        System.out.println( "selam arkadas" );
-        System.out.println( "selam arkadas" );
-        System.out.println( "selam arkadas" );
+  
 
         
 
